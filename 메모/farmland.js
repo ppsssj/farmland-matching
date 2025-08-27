984f8d9dbd6c6a63f5b3a1cc911200db12cf5a5b
ppsssj <psj3154728@gamil.com>
2025-08-27 16:34:34 +0900

    farmland-matching code

---
+++ 메모/farmland.js
@@ -0,0 +1,73 @@
+id: 1,
+      lat: 36.73346211098126,
+      lng: 126.9614054010663,
+      emoji: "🍎",
+      name: "아산 1번 농지",
+      address: "충남 아산시 신창면",
+      crop: "사과", //직전 재배 이력
+      area: 863, //면적
+      price: 13376,
+      Memory:"사연/ 에파소드",
+      Photo:"사진 첨부(최대 5장)",
+      detail: {
+        landInfo: {
+          crop: "사과",
+          areaHectare: "0.9ha",
+          location: "충남 아산시 신창면 북수리 858-4",
+          landNumber: "631-4",
+          soilType: "사질양토", //토양형태
+          waterSource: "인근 하천과 연결됨",
+          owner: "이말순 (75세)",
+          Home:"순천향대", //거주지
+          RoadAdderss: "몰라", //도로명
+          SandQual:"몰라", //토질
+          Resister:"2024-7-31", //등록일
+        },
+        aiProfit: {
+          yearlyProfit: "약 5,176,600원 / 년",
+          yield: "약 4,706kg",
+          unitPrice: "평균 1,100원/kg",
+          cost: {
+            material: "750,000원",
+            labor: "1,200,000원",
+            machine: "420,000원",
+          },
+          netProfit: "2,806,600원 ± 12%",
+        },
+        trustMatch: {
+          status: "매칭 대기 중",
+          preferences: ["배 재배 경험 우대", "인근 거주자 선호"],
+          waitingYouth: 2,
+        },
+        BulidInfo: { //기반시설
+            Facility:"기반시설",
+            Water: "농업용수 접근성",
+            Elec:"전기유무",
+            Mashine:"농기계 접근 가능 여부",
+            Store:"창고/비닐하우스/울타리",
+         },
+         Accessable: { //접근성
+            Access:"접근성",
+            Road: "도로 인접 여부",
+            WellRoad:"포장도로 여부",
+            Bus:"대중교통 접근성",
+            Car:"차량 진입 가능 여부",
+         },
+         TradeWork: { //희망조건(거래관련)
+            Trade:"희망조선(거래관련)",
+            Type: "도로 인접 여부",
+            MAtch:"우선 매칭 대상",
+            Price:"희망 금액",
+            When:"매도 희망 시기",
+         },
+            otherInfo: { //기타 첨부 서류
+            Other:"기타 첨부 서류",
+            Why: "등록 사유",
+            Update1:"등기부등본 업로드",
+            Update2:"토지대장 업로드",
+            Update3:"농지원부/ 경영채 등록증 업로드",
+         },
+        sellerComment:
+          "마을 사람들과 함께 일구던 밭이에요. 공동체를 소중히 여기는 분이었으면 해요.",
+      },
+    },
